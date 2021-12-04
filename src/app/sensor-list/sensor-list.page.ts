564dd5721ad52507999030774f0526e09d1f3d65
Christianloic <43075539+ChristianLoic@users.noreply.github.com>
2021-12-04 01:55:19 -0500

    3/4 page serres-list entamer

--- src/app/sensor-list/sensor-list.page.ts
+++ src/app/sensor-list/sensor-list.page.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+
+import { Serre, serres } from '../serres';
+import { Sensor, sensors } from '../sensors';
 
 @Component({
   selector: 'app-sensor-list',
@@ -7,9 +13,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SensorListPage implements OnInit {
 
-  constructor() { }
 
-  ngOnInit() {
+  @Input() sensor!: Sensor | undefined;
+  sensors = sensors;
+  serres = serres;
+  serre : Serre | undefined;
+
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit()
+  {
+    // First get the serre id from the current route.
+    const routeParams = this.route.snapshot.paramMap;
+    const serreIdFromRoute = Number(routeParams.get('idSerre'));
+
+    // Find the product that correspond with the id provided in route.
+    this.serre = serres.find(serre => serre.idSerre === serreIdFromRoute);
   }
 
 }
---
+++ src/app/sensors.ts
@@ -0,0 +1,121 @@
+export interface Sensor
+{
+  idSensor: number;
+  description: string;
+  valeur : number
+  idSerre:number;
+}
+
+
+export const sensors =
+  [
+    {
+      idSensor: 1,
+      description: 'humidite air',
+      valeur : 30,
+      idSerre:1,
+    },
+    {
+      idSensor: 2,
+      description: 'luminosite',
+      valeur : 30,
+      idSerre:1,
+    },
+    {
+      idSensor: 3,
+      description: 'humidite sol',
+      valeur : 30,
+      idSerre:1,
+    },
+    {
+      idSensor: 1,
+      description: 'humidite air',
+      valeur : 30,
+      idSerre:2,
+    },
+    {
+      idSensor: 2,
+      description: 'luminosite',
+      valeur : 30,
+      idSerre:2,
+    },
+    {
+      idSensor: 3,
+      description: 'humidite sol',
+      valeur : 30,
+      idSerre:2,
+    },
+    {
+      idSensor: 1,
+      description: 'temperature',
+      valeur : 30,
+      idSerre:3,
+    },
+    {
+      idSensor: 2,
+      description: 'luminosite',
+      valeur : 30,
+      idSerre:3,
+    },
+    {
+      idSensor: 3,
+      description: 'humidite sol',
+      valeur : 30,
+      idSerre:3,
+    },
+    {
+      idSensor: 1,
+      description: 'humidite air',
+      valeur : 30,
+      idSerre:3,
+    },
+    {
+      idSensor: 2,
+      description: 'luminosite',
+      valeur : 30,
+      idSerre:3,
+    },
+    {
+      idSensor: 3,
+      description: 'temperature',
+      valeur : 30,
+      idSerre:3,
+    },
+    {
+      idSensor: 1,
+      description: 'humidite air',
+      valeur : 30,
+      idSerre:3,
+    },
+    {
+      idSensor: 2,
+      description: 'luminosite',
+      valeur : 30,
+      idSerre:3,
+    },
+    {
+      idSensor: 3,
+      description: 'humidite sol',
+      valeur : 30,
+      idSerre:3,
+    },
+    {
+      idSensor: 1,
+      description: 'humidite air',
+      valeur : 30,
+      idSerre:3,
+    },
+    {
+      idSensor: 2,
+      description: 'luminosite',
+      valeur : 30,
+      idSerre:3,
+    },
+    {
+      idSensor: 3,
+      description: 'humidite sol',
+      valeur : 30,
+      idSerre:3,
+    }
+  ]
+
