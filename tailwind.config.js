b2d35aea178772f5d1bb1ad9533a50c66dd5929c
Christianloic <43075539+ChristianLoic@users.noreply.github.com>
2021-12-03 21:47:04 -0500

    tailwind fonctionnel

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
 module.exports = {
-  purge: [],
+  purge: {
+    ///enabled: process.env.WEBPACK_DEV_SERVER === 'true' && process.argv.join(' ').indexOf('build') !== -1,
+    content: [
+  "./src/**/*.{html,ts}",
+  "./projects/**/*.{html,ts}"]},
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
   variants: {
-    extend: {},
+    extend: { colors: {
+        'brown':'#795d44',
+        'darkbrown':'#5c4734',
+        'lightblack':'#1d1b1b',
+        'vertBill': '#015C5B',
+      }},
   },
   plugins: [],
 }
