f87d2961527d21b7e3f5603b1c96275dd1445466
1810084 <1810084@W10-INFO-FIX-31>
2021-12-10 15:47:28 -0500

    probleme de bouton a resoudre

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -20,7 +20,7 @@ const routes: Routes = [
     loadChildren: () => import('./map/map.module').then( m => m.MapPageModule)
   },
   {
-    path: 'sensor-details',
+    path: 'sensors/:idSerre',
     loadChildren: () => import('./sensor-details/sensor-details.module').then( m => m.SensorDetailsPageModule)
   },
   {
--- src/app/sensor-details/sensor-details.page.ts
+++ src/app/sensor-details/sensor-details.page.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
+
+import { Sensor , sensors } from '../sensors';
+
 
 @Component({
   selector: 'app-sensor-details',
@@ -7,9 +13,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SensorDetailsPage implements OnInit {
 
-  constructor() { }
+  sensor: Sensor | undefined;
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    const routeParams = this.route.snapshot.paramMap;
+    const sensorIdFromRoute = Number(routeParams.get('idSensor'));
+
+    // Find the sensor that correspond with the id provided in route.
+    this.sensor = sensors.find(sensor => sensor.idSensor === sensorIdFromRoute);
   }
+
+
 
 }
