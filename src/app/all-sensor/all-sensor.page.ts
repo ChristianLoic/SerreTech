ad94a40d3ddcb5fbd065aca4ff944af3bedb9693
1810084 <1810084@W10-INFO-FIX-31>
2021-12-10 14:54:49 -0500

    all sensor fait mais pas router

--- src/app/all-sensor/all-sensor.page.ts
+++ src/app/all-sensor/all-sensor.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { sensors } from '../sensors';
 
 @Component({
   selector: 'app-all-sensor',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./all-sensor.page.scss'],
 })
 export class AllSensorPage implements OnInit {
+
+
+  sensors = sensors;
 
   constructor() { }
 
--- src/app/sensors.ts
+++ src/app/sensors.ts
@@ -2,7 +2,8 @@ export interface Sensor
 {
   idSensor: number;
   description: string;
-  valeur : number
+  valeur : number;
+  img: string;
   idSerre:number;
 }
 
@@ -12,108 +13,126 @@ export const sensors =
     {
       idSensor: 1,
       description: 'humidite air',
+      img:'goutte.png',
       valeur : 30,
       idSerre:1,
     },
     {
       idSensor: 2,
       description: 'luminosite',
+      img: 'soleil.png',
       valeur : 30,
       idSerre:1,
     },
     {
       idSensor: 3,
       description: 'humidite sol',
+      img:'goutte.png',
       valeur : 30,
       idSerre:1,
     },
     {
       idSensor: 1,
       description: 'humidite air',
+      img:'goutte.png',
       valeur : 30,
       idSerre:2,
     },
     {
       idSensor: 2,
       description: 'luminosite',
+      img: 'soleil.png',
       valeur : 30,
       idSerre:2,
     },
     {
       idSensor: 3,
       description: 'humidite sol',
+      img:'goutte.png',
       valeur : 30,
       idSerre:2,
     },
     {
       idSensor: 1,
       description: 'temperature',
+      img: 'thermometre.png',
       valeur : 30,
       idSerre:3,
     },
     {
       idSensor: 2,
       description: 'luminosite',
+      img: 'soleil.png',
       valeur : 30,
       idSerre:3,
     },
     {
       idSensor: 3,
       description: 'humidite sol',
+      img:'goutte.png',
       valeur : 30,
       idSerre:3,
     },
     {
       idSensor: 1,
       description: 'humidite air',
+      img:'goutte.png',
       valeur : 30,
       idSerre:3,
     },
     {
       idSensor: 2,
       description: 'luminosite',
+      img: 'soleil.png',
       valeur : 30,
       idSerre:3,
     },
     {
       idSensor: 3,
       description: 'temperature',
+      img: 'thermometre.png',
       valeur : 30,
       idSerre:3,
     },
     {
       idSensor: 1,
       description: 'humidite air',
+      img:'goutte.png',
       valeur : 30,
       idSerre:3,
     },
     {
       idSensor: 2,
       description: 'luminosite',
+      img: 'soleil.png',
       valeur : 30,
       idSerre:3,
     },
     {
       idSensor: 3,
       description: 'humidite sol',
+      img:'goutte.png',
       valeur : 30,
       idSerre:3,
     },
     {
       idSensor: 1,
       description: 'humidite air',
+      img:'goutte.png',
       valeur : 30,
       idSerre:3,
     },
     {
       idSensor: 2,
       description: 'luminosite',
+      img: 'soleil.png',
       valeur : 30,
       idSerre:3,
     },
     {
       idSensor: 3,
       description: 'humidite sol',
+      img:'goutte.png',
       valeur : 30,
       idSerre:3,
     }
